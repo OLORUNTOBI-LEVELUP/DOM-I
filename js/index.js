872ebece112e720392ff09922bbdb9599121b5b5
oloruntobi <awoderuoloruntobi@gmail.com>
2019-05-13 15:20:43 +0100

    Replaced all the texts with JSON object

--- js/index.js
+++ js/index.js
@@ -43,87 +43,87 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 //Images
 let firstimage = document.querySelector("#cta-img");
-firstimage.setAttribute("src", "img/header-img.png");
+firstimage.setAttribute("src", siteContent.cta["img-src"]);
 
 let secondimage = document.querySelector("#middle-img");
-secondimage.setAttribute("src", "img/mid-page-accent.jpg");
+secondimage.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
 
 //navbar
 let navcontent = document.querySelector("nav a");
 navcontent.textContent = siteContent.nav["nav-item-1"];
 
 let navcontent2 = document.querySelector("nav a:nth-of-type(2)")
-navcontent2.textContent = "Product";
+navcontent2.textContent = siteContent.nav["nav-item-2"];
 
 let navcontent3 = document.querySelector(" nav a:nth-of-type(3)")
-navcontent3.textContent = "Vision"
+navcontent3.textContent = siteContent.nav["nav-item-3"]
 
 let navcontent4 = document.querySelector(" nav a:nth-of-type(4)")
-navcontent4.textContent = "Features"
+navcontent4.textContent = siteContent.nav["nav-item-4"]
 
 let navcontent5 = document.querySelector(" nav a:nth-of-type(5)")
-navcontent5.textContent = "About"
+navcontent5.textContent = siteContent.nav["nav-item-5"]
 
 let navcontent6 = document.querySelector(" nav a:nth-of-type(6)")
-navcontent6.textContent = "Contact"
+navcontent6.textContent = siteContent.nav["nav-item-6"]
 
 //top content
 let h1 = document.querySelector("h1");
-h1.textContent = "DOM Is Awesome"
+h1.textContent = siteContent.cta.h1
 
 let button = document.querySelector("button");
-button.textContent = "Get Started"
+button.textContent = siteContent.cta.button
 
 //middleconetent
 let heading1 = document.querySelector(".text-content h4")
-heading1.textContent = "Features"
+heading1.textContent = siteContent["main-content"]["features-h4"]
 
 let firstContent = document.querySelector(".text-content p")
-firstContent.textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+firstContent.textContent = siteContent["main-content"]["features-content"]
 
 let heading2 = document.querySelector(".text-content:nth-of-type(2) h4")
-heading2.textContent = "About"
+heading2.textContent = siteContent["main-content"]["about-h4"]
 
 let secondContent = document.querySelector(".text-content:nth-of-type(2) p")
-secondContent.textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+secondContent.textContent = siteContent["main-content"]["about-content"]
 
 //bottomcontent 
 let heading3 = document.querySelector(".main-content .bottom-content .text-content:nth-of-type(1) h4");
-heading3.textContent = "Services"
+heading3.textContent = siteContent["main-content"]["services-h4"]
 
 let thirdContent = document.querySelector(".main-content .bottom-content .text-content:nth-of-type(1) p")
-thirdContent.textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+thirdContent.textContent = siteContent["main-content"]["services-content"]
 
 let heading4 = document.querySelector(".main-content .bottom-content .text-content:nth-of-type(2) h4");
-heading4.textContent = "Product"
+heading4.textContent = siteContent["main-content"]["product-h4"]
 
 let fourthContent = document.querySelector(".main-content .bottom-content .text-content:nth-of-type(2) p")
-fourthContent.textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+fourthContent.textContent = siteContent["main-content"]["product-content"]
 
 let heading5 = document.querySelector(".main-content .bottom-content .text-content:nth-of-type(3) h4");
-heading5.textContent = "Vision"
+heading5.textContent = siteContent["main-content"]["vision-h4"]
 
 let fifthContent = document.querySelector(".main-content .bottom-content .text-content:nth-of-type(3) p")
-fifthContent.textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+fifthContent.textContent = siteContent["main-content"]["vision-content"]
 
 //contact
 
 let contact = document.querySelector(".contact h4")
-contact.textContent = "Contact"
+contact.textContent = siteContent.contact["contact-h4"]
 
 let paragraph1 = document.querySelector(".contact p:nth-of-type(1)")
-paragraph1.textContent = "123 Way 456 Street Somewhere, USA"
+paragraph1.textContent = siteContent.contact.address
 
 let paragraph2 = document.querySelector(".contact p:nth-of-type(2)")
-paragraph2.textContent = "1 (888) 888-8888"
+paragraph2.textContent = siteContent.contact.phone
 
 let paragraph3 = document.querySelector(".contact p:nth-of-type(3)")
-paragraph3.textContent = "sales@greatidea.io"
+paragraph3.textContent = siteContent.contact.email
 
 
 //footer
 let footer = document.querySelector("footer")
-footer.textContent = "Copyright Great Idea! 2018"
+footer.textContent = siteContent.footer.copyright
 
 
 
